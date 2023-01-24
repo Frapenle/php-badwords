aff5650a41805943c2a916b963468f49e80c9f83
francescotrudu <francescotrudu84@gmail.com>
2023-01-24 16:56:40 +0100

    Add textarea, input and button

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?php
-$test = 'hello world';
-echo $test;
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+    <form action="./form.php">
+        <label for="badword">Inserire parola da nascondere</label>
+        <input type="text" name="badword" id="badword">
+        <button type="submit">Replace</button>
+        <br>
+        <br>
+        <label for="sentence">Inserire del testo</label>
+        <br>
+        <textarea type="text" name="sentence" id="sentence" method="GET" rows="4" cols="55"></textarea>
+    </form>
+
+
+
+</body>
+
+</html>
